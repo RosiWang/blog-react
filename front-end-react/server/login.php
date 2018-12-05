27d8ff9d3803812985d6481ac00f602ff66d2643
Rosi <rosiwang@163.com>
2018-12-05 18:44:31 +0800

    调整文件结构

--- front-end-react/server/login.php
+++ front-end-react/server/login.php
@@ -6,7 +6,7 @@ if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }else{
-	 
+
 	 mysql_select_db('blog',$con);//选中要操作的数据库
 	 mysql_set_charset('utf8',$con);
 	 $sql = 'select * from artical order by id asc';
@@ -14,14 +14,15 @@ if (!$con)
 	  $result = mysql_query($sql,$con);
 	   for($i=0; $i<2; $i++){
                     $row = mysql_fetch_assoc($result);
-                     array_push($sqlData, $row);                    
+                     array_push($sqlData, $row);
                 }
+
 
    // header("Content-type:text/html;charset=utf-8");
 
     // echo json_encode(array('name' => '孙晓辉', 'age' => 23),JSON_UNESCAPED_UNICODE);
      echo json_encode($sqlData,JSON_UNESCAPED_UNICODE);
-    
-	  
+
+
   }
 ?>
