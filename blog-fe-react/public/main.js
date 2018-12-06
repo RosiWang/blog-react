306bc4576febe4bb8913587594ad9289faedc71a
Rosi <rosiwang@163.com>
2018-12-06 14:25:37 +0800

    目录结构调整

---
+++ blog-fe-react/public/main.js
@@ -0,0 +1,10 @@
+//main.js
+// const greeter = require('./HomePage.js');
+// document.querySelector("#root").appendChild(greeter());
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "../src/App";
+
+console.log('main start..');
+ReactDOM.render(<App />, document.getElementById('root'))
