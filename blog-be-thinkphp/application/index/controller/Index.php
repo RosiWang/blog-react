f41527f98d49e29a9c838ccf21f32dbf07729a79
Rosi <rosiwang@163.com>
2018-12-07 17:22:19 +0800

    '连接数据库'

--- blog-be-thinkphp/application/index/controller/Index.php
+++ blog-be-thinkphp/application/index/controller/Index.php
@@ -1,26 +1,49 @@
 <?php
 namespace app\index\controller;
+use think\Controller;
+use think\Db;
 
-class Index
+class Index extends Controller
 {
-    public function index($name = 'test')
+    public function index()
     {
-//        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">测试测试测试测试788444</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
-//       $data = array('name' => '王某', 'age' => 23);
-        return  'test' .$name .'8566';
+        $data = Db::name('think_data')->find();
+        $this->assign('result', $data);
+        return $this->fetch();
+    }
+
+    public function hello($name = 'thinkphp'){
+        $this -> assign('name',$name);
+        return  $this->fetch();
     }
 
     public function test(){
-        return 'public 方法';
+        return 'public 方法 test';
     }
 
-    private function hello()
+    public function sqlTest()
     {
-        return 'private 方法';
-    }
-
-    protected function hello1(){
-        return 'protected 方法';
+        $con = mysql_connect("localhost","root","root");
+        $sqlData = array( );
+        $returnStr = '';
+        if (!$con)
+        {
+            die('Could not connect: ' . mysql_error());
+            $sqlData = array('code'=>-1);
+            $returnStr = '连接失败  '.json_encode($sqlData,JSON_UNESCAPED_UNICODE);
+        }else{
+            mysql_select_db('blog',$con);//选中要操作的数据库
+            mysql_set_charset('utf8',$con);
+            $sql = 'select * from artical order by id asc';
+            $result = mysql_query($sql,$con);
+            $len = mysql_num_rows($result);
+            for($i=0; $i<$len; $i++){
+                $row = mysql_fetch_assoc($result);
+                array_push($sqlData, $row);
+            }
+            $returnStr = '连接成功  '.json_encode($sqlData,JSON_UNESCAPED_UNICODE);
+        }
+        return $returnStr;
     }
 
 }
